253f178d0b9ac4eb61c443c37549af0ed547387e
rabbitblood <qz154772610@gmail.com>
2024-09-06 22:01:44 -0700

    fix state display wrong

--- services/contract/memepair-contract.ts
+++ services/contract/memepair-contract.ts
@@ -498,8 +498,15 @@ export class MemePairContract implements BaseContract {
       return;
     }
 
+    console.log(
+      this.depositedRaisedToken.toNumber(),
+      this.endTime,
+      this.raisedTokenMinCap.div(Math.pow(10, 18)).toNumber()
+    );
+
     if (
-      this.depositedRaisedToken >= this.raisedTokenMinCap.div(Math.pow(10, 18))
+      this.depositedRaisedToken.toNumber() >=
+      this.raisedTokenMinCap.div(Math.pow(10, 18)).toNumber()
     ) {
       this.ftoState = 0;
     } else if (dayjs.unix(Number(this.endTime)).isBefore(dayjs())) {
