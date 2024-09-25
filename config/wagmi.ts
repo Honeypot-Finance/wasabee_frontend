72a84a524af032b9b8f0865a3e14786cb0e2b928
rabbitblood <qz154772610@gmail.com>
2024-09-24 21:53:57 -0700

    implement bitget wallet

--- config/wagmi.ts
+++ config/wagmi.ts
@@ -1,10 +1,40 @@
+import { networks } from "@/services/chain";
+import { connectorsForWallets, getDefaultConfig } from "@rainbow-me/rainbowkit";
 
-import { networks } from '@/services/chain';
-import { getDefaultConfig } from '@rainbow-me/rainbowkit';
+import {
+  rainbowWallet,
+  bitgetWallet,
+  okxWallet,
+  walletConnectWallet,
+} from "@rainbow-me/rainbowkit/wallets";
+
+//for users without bitget wallet
+let customWallets = [
+  rainbowWallet,
+  walletConnectWallet,
+  bitgetWallet,
+  okxWallet,
+];
+// if(!window.bitkeep){
+//   customWallets.unshift(bitgetWallet);
+// }
+const connectors = connectorsForWallets(
+  [
+    {
+      groupName: "Recommended",
+      wallets: customWallets,
+    },
+  ],
+  {
+    appName: "Honypot Finance",
+    projectId: "1d1c8b5204bfbd57502685fc0934a57d",
+  }
+);
 
 export const config = getDefaultConfig({
-  appName: 'honeypotfinance',
-  projectId: '1d1c8b5204bfbd57502685fc0934a57d',
+  connectors,
+  appName: "Honypot Finance",
+  projectId: "1d1c8b5204bfbd57502685fc0934a57d",
   // @ts-ignore
   chains: networks.map((network) => network.chain),
   ssr: true, // If your dApp uses server side rendering (SSR)
