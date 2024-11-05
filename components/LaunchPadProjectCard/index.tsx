b184d9b173ddb5b3b1fdf9c0fe9da58dba205a06
minh duc nguyen <minhducnguyenpsy@gmail.com>
2024-11-05 18:12:07 +0700

    feat/lbp's-projects-page

--- components/LaunchPadProjectCard/index.tsx
+++ components/LaunchPadProjectCard/index.tsx
@@ -2,12 +2,15 @@ import clsx from "clsx";
 import { observer } from "mobx-react-lite";
 import React from "react";
 import Image from "next/image";
+import Countdown from "react-countdown";
+import { Button } from "@/components/button";
 
 type IProjectCardStatus = "live" | "comming";
 
 type ILaunchPadProjectCard = {
   status: IProjectCardStatus;
-  coverImg: string;
+  coverImg: string | null;
+  isShowCoverImage?: boolean;
 };
 
 const ProjectCardStatus = observer(
@@ -34,7 +37,7 @@ const ProjectCardStatus = observer(
           ></div>
         </div>
         <div className="font-bold text-white text-[1rem] leading-[1.3rem] min-w-[110px] text-center">
-          {status == "live" ? "Live now" : "Comming soon"}
+          {status == "live" ? "Live now" : "Coming Soon"}
         </div>
       </div>
     );
@@ -52,26 +55,18 @@ const TokenInfo = observer(
   ({ symbol, name, author, description }: ITokenInfo) => {
     return (
       <div className="flex flex-col gap-[6px]">
-        <div className="flex">
+        <div className="flex gap-[5px] items-center">
           <div className="w-[52px] h-[52px] rounded-full overflow-hidden relative">
             <Image src={symbol} fill alt={name} />
           </div>
           <div className="">
-            <div>{name}</div>
-            <div>{author}</div>
+            <div className="text-base leading-[20px] font-bold text-white">{name}</div>
+            <div className="text-base leading-[20px] font-bold text-[#FFFFFF8C]">{author}</div>
           </div>
         </div>
         {description && (
           <p className="line-clamp-2 text-sm leading-[18px] text-[#FFFFFF8C]">
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry&lsquo;s standard dummy
-            text ever since the 1500s, when an unknown printer took a galley of
-            type and scrambled it to make a type specimen book. It has survived
-            not only five centuries, but also the leap into electronic
-            typesetting, remaining essentially unchanged. It was popularised in
-            the 1960s with the release of Letraset sheets containing Lorem Ipsum
-            passages, and more recently with desktop publishing software like
-            Aldus PageMaker including versions of Lorem Ipsum
+            {description}
           </p>
         )}
       </div>
@@ -79,22 +74,94 @@ const TokenInfo = observer(
   }
 );
 
-const LaunchPadProjectCard = observer(({ status }: ILaunchPadProjectCard) => {
+type ILaunchPadProjectBody = {
+  status:IProjectCardStatus
+  totalRaised: number
+  symbol?: string;
+  endDate: number;
+}
+
+const LaunchPadProjectBody = observer(({status,totalRaised, endDate,symbol='tHpot'}: ILaunchPadProjectBody) => {
+
+   
+
+  return <div className="my-[10px] py-[8px] flex items-center flex-col justify-between w-full bg-[#31220C] rounded-2xl gap-[18px]">
+    <ProjectCardStatus status={status} />
+
+    <Countdown  date={endDate} renderer={({ days, hours, minutes, seconds, completed }) => {
+      if(completed){
+        return <div className="text-center font-bold">
+        <div className="text-xl leading-[26px]">{totalRaised} {symbol}</div>
+        <div className="mt-[5px] text-[9px] leading-[11px] text-[#FFFFFF70] ">Total Raised</div>
+      </div>
+      }else{
+        console.log({days, hours, minutes, seconds, completed})
+         return <div className="font-bold text-lg flex items-center gap-[14px]">
+          <div className="flex flex-col text-center gap-[5px]">
+            <span className="leading-[26px]">{days > 0 ? days : '00'}</span>
+            <span className="text-[9px] leading-3 text-[#FFFFFF70]">{days > 1 ? "Days" : "Day"}</span>
+          </div>
+          <div>:</div>
+          <div className="flex flex-col text-center gap-[5px]">
+            <span className="leading-[26px]">{hours > 0 ? hours : '00'}</span>
+            <span className="text-[9px] leading-3 text-[#FFFFFF70]">{hours > 1 ? "Hours" : "Hour"}</span>
+          </div>
+          <div>:</div>
+          <div className="flex flex-col text-center gap-[5px]">
+            <span className="leading-[26px]">{minutes > 0 ? minutes : '00'}</span>
+            <span className="text-[9px] leading-3 text-[#FFFFFF70]">{minutes > 1 ? "Minutes" : "Minute"}</span>
+          </div>
+          <div>:</div>
+          <div className="flex flex-col text-center gap-[5px]">
+            <span className="leading-[26px]">{seconds > 0 ? seconds : '00'}</span>
+            <span className="text-[9px] leading-3 text-[#FFFFFF70]">{seconds > 1 ? "Secs" : "Sec"}</span>
+          </div>
+          </div>;
+      }
+    }} />
+    
+  </div>
+})
+
+const LaunchPadProjectCard = observer(({ status, coverImg,isShowCoverImage = false }: ILaunchPadProjectCard) => {
+
+  const endDate = status == "comming" ? Date.now() + 50000000 : Date.now() - 1000
+
   return (
     <div
       className={clsx(
-        "max-w-[18.5rem] w-full rounded-[20px] bg-[#1D1407] border border-[#F7931A0D] overflow-hidden"
+        "min-w-[18.5rem]   rounded-[20px] bg-[#1D1407] border border-[#F7931A0D] overflow-hidden"
       )}
     >
-      <div className="h-[78px] relative w-full bg-[radial-gradient(ellipse_at_center, #FFCD4D, #83C2E9)]">
-        <Image
+      {isShowCoverImage && <div className="h-[78px] relative w-full bg-[radial-gradient(at_center,#FFCD4D,#83C2E9)]">
+        {coverImg && coverImg?.length > 0 && <Image
           alt="Cover Image"
-          src="https://images.unsplash.com/photo-1730456588662-3d95038308f9?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          src={coverImg}
           className="object-fit "
           fill={true}
+        />}
+      </div>}
+      <div className="p-[15px] pb-0">
+        <TokenInfo
+          symbol="/images/icons/tokens/thpot-token-yellow-icon.png"
+          name="Yellow Honey"
+          author="Huny"
+          // description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry."
         />
+        <LaunchPadProjectBody endDate={endDate} status={status} totalRaised={3389}/>
+        
       </div>
-      <ProjectCardStatus status={status} />
+      <div className="p-[10px]">
+        {status === "live" ? 
+        <Button className="w-full outline-2">
+          <span className="font-bold text-[12px]">Buy Token</span>
+        </Button>
+        :
+        <Button className="w-full outline-2">
+          <span className="font-bold text-[12px]">View Token</span>
+        </Button>
+        }
+        </div>
     </div>
   );
 });
--- pages/launchpad-projects.tsx
+++ pages/launchpad-projects.tsx
@@ -33,6 +33,7 @@ const MemeLaunchPage: NextLayoutPage = observer(() => {
   const [mostSuccessProjects, setMostSuccessProjects] = useState<
     MemePairContract[] | null
   >(null);
+  const [selectedTab, setSelectedTab] = useState<'all' | 'my'>('all')
 
   useEffect(() => {
     if (!wallet.isInit) {
@@ -51,19 +52,11 @@ const MemeLaunchPage: NextLayoutPage = observer(() => {
     });
   }, [wallet.isInit]);
 
-  return (
-    <div className="px-6 xl:max-w-[1200px] mx-auto flex flex-col sm:gap-y-4">
-      <div className="flex w-full justify-end gap-2">
-        <Button className="scale-[0.8] sm:scale-100">
-          <Link
-            href="/launch-token?launchType=meme"
-            className="text-black font-bold"
-          >
-            Launch Token
-          </Link>
-        </Button>
-      </div>
+  console.log("launchpad.projectsPage", launchpad.projectsPage)
 
+  return (
+    <div className="px-7 xl:max-w-[1280px] mx-auto flex flex-col sm:gap-y-4">
+     
       {mostSuccessProjects && mostSuccessProjects.length > 0 && (
         <>
           <h2 className="w-full text-center text-[1.75rem] leading-4 font-bold text-neutral-100 ">
@@ -73,129 +66,35 @@ const MemeLaunchPage: NextLayoutPage = observer(() => {
             variants={defaultContainerVariants}
             initial="hidden"
             animate="visible"
-            className="w-full flex flex-col lg:flex-row gap-2 flex-grow-[1]"
+            className="w-full flex flex-col lg:flex-row gap-4 flex-grow-[1] mt-8"
           >
-            {/* {mostSuccessProjects.map((pair: MemePairContract, idx) => (
-              <motion.div
-                variants={itemPopUpVariants}
-                key={pair.address}
-                className={
-                  "relative flex-grow " + (idx !== 0 && "hidden lg:block")
-                }
-              >
-                <LaunchCard
-                  pair={pair}
-                  action={<></>}
-                  type="trending"
-                  className="p-0"
-                />
-              </motion.div>
-            ))} */}
             <LaunchPadProjectCard status="comming" coverImg={""} />
+            <LaunchPadProjectCard status="live" coverImg={""} />
+            <LaunchPadProjectCard status="live" coverImg={""} />
+            <LaunchPadProjectCard status="live" coverImg={""} />
           </motion.div>
         </>
       )}
 
-      <div>
-        <div id="filter" className="flex flex-col sm:flex-row gap-2">
-          <WarppedNextInputSearchBar
-            onChange={(e) => {
-              launchpad.pairFilterSearch = e.target.value;
-            }}
-          />
-          <Popover
-            shouldBlockScroll
-            isOpen={isOpen}
-            onOpenChange={(isOpen) => {
-              isOpen ? onOpen() : onClose();
-            }}
-            placement="bottom"
-            classNames={{
-              base: [
-                // arrow color
-                "before:bg-default-200",
-              ],
-              content: [
-                "py-3 px-4 border border-default-200",
-                "bg-gradient-to-br from-white to-default-300",
-                "dark:from-default-100 dark:to-default-50",
-              ],
-            }}
-          >
-            <PopoverTrigger>
-              <NextButton className="inline-flex w-full sm:w-[124px] h-10 justify-between items-center shrink-0 border [background:#3E2A0F] px-2.5 py-0 rounded-[30px] border-solid border-[rgba(247,147,26,0.10)] text-white text-center">
-                <span className="flex-1">
-                  {launchpad.projectsPage.filter.status.toUpperCase()}
-                </span>
-                <DropdownSvg></DropdownSvg>
-              </NextButton>
-            </PopoverTrigger>
-            <PopoverContent className="flex lg:w-[352px] flex-col items-center gap-4 border border-[color:var(--card-stroke,#F7931A)] [background:var(--card-color,#271A0C)] rounded-xl border-solid">
-              <Observer>
-                {() => (
-                  <div className="w-full">
-                    <SpinnerContainer
-                      isLoading={launchpad.projectsPage.isLoading}
-                    >
-                      <div className="max-h-[300px] grid grid-cols-3 gap-2">
-                        <NextButton
-                          onClick={() => {
-                            launchpad.pairFilterStatus = "all";
-                          }}
-                          className="w-[100px]"
-                        >
-                          All
-                        </NextButton>
-                        <NextButton
-                          onClick={() => {
-                            launchpad.pairFilterStatus = "success";
-                          }}
-                          className="w-[100px]"
-                        >
-                          Success
-                        </NextButton>
-                        <NextButton
-                          onClick={() => {
-                            launchpad.pairFilterStatus = "fail";
-                          }}
-                          className="w-[100px]"
-                        >
-                          Failed
-                        </NextButton>
-                        <NextButton
-                          onClick={() => {
-                            console.log("processing");
-                            launchpad.pairFilterStatus = "processing";
-                          }}
-                          className="w-[100px]"
-                        >
-                          Processing
-                        </NextButton>
-                      </div>
-                    </SpinnerContainer>
-                  </div>
-                )}
-              </Observer>
-            </PopoverContent>
-          </Popover>
-        </div>
-      </div>
-
-      <div>
+      <div className="flex justify-between">
         <Tabs
-          // destroyInactiveTabPanel={false}
           aria-label="Options"
+          variant="light"
+          radius="sm"
+          size="lg"
           classNames={{
-            tabList: "bg-transparent",
-            tab: "flex flex-col items-center gap-2.5 border-0  backdrop-blur-[100px] p-2.5 rounded-[10px]",
+            tab: "font-bold text-base leading-4 data-[selected=true]:bg-[#E18A2066] data-[selected=true]:backdrop-blur-[200] data-[selected=true]:border-1 data-[selected=true]:border-[#E18A2066s]",
+            tabContent: "group-data-[selected=true]:text-[#ffffff]",
+            cursor: "group-data-[selected=true]:bg-[#E18A2066]"
           }}
-          className="next-tab"
           onSelectionChange={(key) => {
             launchpad.setCurrentLaunchpadType("meme");
             if (key === "all") {
+              setSelectedTab(key)
               launchpad.projectsPage.setIsInit(false);
               launchpad.pairFilterStatus = defaultPairFilters.all.status;
             } else if (key === "my") {
+              setSelectedTab(key)
               launchpad.myLaunches.setIsInit(false);
               launchpad.pairFilterStatus = defaultPairFilters.myPairs.status;
             } else if (key === "participated-launch") {
@@ -205,57 +104,49 @@ const MemeLaunchPage: NextLayoutPage = observer(() => {
             }
           }}
         >
-          <Tab key="all" title="All MEMEs">
-            <Pagination
-              paginationState={launchpad.projectsPage}
-              render={(pair) => <LaunchCard pair={pair} action={<></>} />}
-              classNames={{
-                itemsContainer:
-                  "grid gap-8 grid-cols-1 md:grid-cols-2 xl:gap-6 xl:grid-cols-3",
-              }}
-            />
-          </Tab>
-          <Tab key="my" title="My MEMEs">
-            <Pagination
-              paginationState={launchpad.myLaunches}
-              render={(pair) => <LaunchCard pair={pair} action={<></>} />}
-              classNames={{
-                itemsContainer:
-                  "grid gap-8 grid-cols-1 md:grid-cols-2 xl:gap-6 xl:grid-cols-3",
-              }}
-            />
-          </Tab>
-          <Tab key="participated-launch" title="Participated MEMEs">
-            <Pagination
-              paginationState={launchpad.participatedPairs}
-              render={(pair) => <LaunchCard pair={pair} action={<></>} />}
-              classNames={{
-                itemsContainer:
-                  "grid gap-8 grid-cols-1 md:grid-cols-2 xl:gap-6 xl:grid-cols-3",
-              }}
-            />
-          </Tab>
-          {/* <Tab href="/launch" title="To Fto projects->" /> */}
-          <Tab
-            href="https://bartio.bonds.yeetit.xyz/"
-            target="_blank"
-            title={
-              <div className="flex items-center text-yellow-400">
-                <Image
-                  className="h-4"
-                  src="/images/partners/yeet_icon.png"
-                  alt=""
-                  width={100}
-                  height={100}
-                />
-                <span className="flex items-center justify-center gap-2">
-                  Try Yeet Bond <FaExternalLinkAlt className="inline-block" />
-                </span>
-              </div>
-            }
-          />
+          
+            <Tab key="all" title="All Projects" />
+            <Tab key="my" title="My Projects" />
         </Tabs>
+
+        <div className="flex gap-5">
+        <WarppedNextInputSearchBar
+        className="max-w-[305px] w-[305px] h-[46px] flex"
+            onChange={(e) => {
+              launchpad.pairFilterSearch = e.target.value;
+            }}
+          />
+         
+        <Button className="px-[38px] py-[12.5px] rounded-full outline-1 !bg-[#FFCD4D] border-2 border-[#E18A2066]">
+          <Link
+            href="/launch-token?launchType=meme"
+            className="text-black font-bold text-sm leading-4"
+          >
+            Launch Token
+          </Link>
+        </Button>
+        </div>
       </div>
+
+      <div>
+            {selectedTab === 'all' ? <Pagination
+              paginationState={launchpad.projectsPage}
+              render={(pair) => <LaunchPadProjectCard status={"live"} coverImg={''}  isShowCoverImage={true} />}
+              classNames={{
+                itemsContainer:
+                  "w-full flex flex-col lg:flex-row gap-4",
+              }}
+            />:<Pagination
+            paginationState={launchpad.myLaunches}
+            render={(pair) => <LaunchPadProjectCard status={"live"} coverImg={''} isShowCoverImage={true}  />}
+            classNames={{
+              itemsContainer:
+                "flex flex-wrap gap-4",
+            }}
+          /> }
+          </div>
+    
+   
     </div>
   );
 });
