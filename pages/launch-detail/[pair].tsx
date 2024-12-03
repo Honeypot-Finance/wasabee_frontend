536f81e87566a9b858d011eddb33801cdf339d32
AricRedemption <aricredemption@gmail.com>
2024-12-04 04:30:59 +0800

    chore: add image

--- pages/launch-detail/[pair].tsx
+++ pages/launch-detail/[pair].tsx
@@ -43,6 +43,23 @@ import {
 import { LuFileEdit } from "react-icons/lu";
 import { zeroAddress } from "viem";
 
+const IrregularBorder = () => {
+  return (
+    <div className="bg-[#FFCD4D]">
+  <div
+      className="relative w-80 h-80 bg-white overflow-hidden"
+      style={{
+        clipPath: `path('M0,60 C10,55 15,30 25,45 C30,20 45,70 50,55 C60,40 70,85 80,60 C85,35 90,65 100,50 C110,20 120,80 125,45 C130,30 140,75 150,60 C160,20 170,85 175,50 C180,40 190,70 200,55 C210,25 220,80 230,60 C240,35 250,75 260,50 C270,30 280,85 290,55 C300,40 310,80 320,50 C330,20 340,65 350,55 C360,40 370,70 380,50 C390,25 400,85 410,60 C420,35 430,80 440,55 C450,25 460,75 470,50 C480,30 490,70 500,55 C510,35 520,80 530,60 C540,25 550,90 560,100 L560,300 L0,300 Z')`,
+      }}
+    >
+      <div className="absolute inset-0 flex items-center justify-center text-white text-lg font-semibold">
+        Irregular Border
+      </div>
+    </div>
+    </div>
+  );
+};
+
 const UpdateProjectModal = observer(
   ({ pair }: { pair: FtoPairContract | MemePairContract }) => {
     const {
@@ -670,6 +687,7 @@ const MemeView = observer(() => {
 
   return (
     <div className="px-2 md:px-6 xl:max-w-[1200px] mx-auto pb-[20vh]">
+      <IrregularBorder />
       {state.pair.value && (
         <Modal
           isOpen={isOpen}
