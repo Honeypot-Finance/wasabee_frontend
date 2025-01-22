93fe08abd09421912dc4435d4260b24e81358e25
rabbitblood <qz154772610@gmail.com>
2025-01-21 16:39:34 -0800

    add powered by algebra an wasabee below swap component

--- components/algebra/common/PoweredByAlgebra/index.tsx
+++ components/algebra/common/PoweredByAlgebra/index.tsx
@@ -1,21 +1,48 @@
-import AlgebraLogo from "@/assets/algebra/algebra-logo.svg";
-import AlgebraIntegral from "@/assets/algebra/algebra-itegral.svg";
+import AlgebraLogo from "@/public/images/partners/algebra_logo.svg";
+import WasabeeLogo from "@/public/images/partners/wasabee.png";
 import { cn } from "@/lib/tailwindcss";
-
+import Link from "next/link";
+import Image from "next/image";
+import { Tooltip } from "@nextui-org/react";
 const PoweredByAlgebra = ({ className }: { className?: string }) => {
   return (
-    <a
-      href={"https://algebra.finance"}
-      className={cn("flex items-center gap-2 p-2", className)}
+    <div
+      className={cn("flex items-center justify-center gap-2 p-2", className)}
     >
       <span className="text-sm font-semibold">Powered by</span>
       <div className="flex items-center gap-1">
-        <div className="flex items-center justify-center w-[18px] h-[18px] rounded-full">
-          <img src={AlgebraLogo} width={18} height={18} />
-        </div>
-        <img src={AlgebraIntegral} width={120} height={18} />
+        <Link
+          href={"https://algebra.finance"}
+          target="_blank"
+          className="flex items-center gap-1"
+        >
+          <Tooltip content="Algebra Integral">
+            <Image
+              src={AlgebraLogo}
+              width={24}
+              height={24}
+              alt="Algebra Logo"
+            />
+          </Tooltip>
+        </Link>
+        <span>&</span>
+        <Link
+          href={"https://x.com/WasabeeFi"}
+          target="_blank"
+          className="flex items-center gap-1"
+        >
+          {/* <span>Wasabee</span> */}
+          <Tooltip content="Wasabee Finance">
+            <Image
+              src={WasabeeLogo}
+              width={24}
+              height={24}
+              alt="Wasabee Logo"
+            />
+          </Tooltip>
+        </Link>
       </div>
-    </a>
+    </div>
   );
 };
 
