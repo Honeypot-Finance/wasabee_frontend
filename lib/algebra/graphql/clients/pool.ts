8119fa88b6c4162c8439e9f76fa6372ca4baac11
rabbitblood <qz154772610@gmail.com>
2025-01-28 21:09:52 -0800

    fix my pools fee calculation make loading slow issue

--- lib/algebra/graphql/clients/pool.ts
+++ lib/algebra/graphql/clients/pool.ts
@@ -103,99 +103,90 @@ export const useUserPools = (userAddress: string) => {
   });
 
   useEffect(() => {
-    if (!data || !wallet.isInit || !algebraPositionManager.simulate || isLoaded)
-      return;
+    if (!data || !wallet.isInit || !algebraPositionManager.simulate) return;
+    data.positions.forEach(async (position) => {
+      if (fetchedPositions.includes(position.pool.id.concat("-", position.id)))
+        return;
+      try {
+        const pool = position.pool;
+        const {
+          result: [fees0, fees1],
+        } = await algebraPositionManager.simulate.collect([
+          {
+            tokenId: BigInt(position.id),
+            recipient: wallet.account as `0x${string}`,
+            amount0Max: MAX_UINT128,
+            amount1Max: MAX_UINT128,
+          },
+        ]);
 
-    setIsLoaded(true);
+        const fees0USD = CurrencyAmount.fromRawAmount(
+          unwrappedToken(
+            new AlgebranToken(
+              wallet.currentChainId,
+              pool.token0.id,
+              Number(pool.token0.decimals),
+              pool.token0.symbol,
+              pool.token0.name
+            )
+          ),
+          fees0.toString()
+        );
 
-    const newPools: Record<string, Pool & { fees: BigNumber }> = {};
-    Promise.all(
-      data.positions.map(async (position) => {
+        const fees1USD = CurrencyAmount.fromRawAmount(
+          unwrappedToken(
+            new AlgebranToken(
+              wallet.currentChainId,
+              pool.token1.id,
+              Number(pool.token1.decimals),
+              pool.token1.symbol,
+              pool.token1.name
+            )
+          ),
+          fees1.toString()
+        );
+
+        const fees0USDformatted = fees0USD
+          ? Number(fees0USD.toSignificant()) * Number(pool.token0.derivedUSD)
+          : 0;
+        const fees1USDformatted = fees1USD
+          ? Number(fees1USD.toSignificant()) * Number(pool.token1.derivedUSD)
+          : 0;
+
         if (
           fetchedPositions.includes(position.pool.id.concat("-", position.id))
         )
           return;
-        try {
-          const pool = position.pool;
-          const {
-            result: [fees0, fees1],
-          } = await algebraPositionManager.simulate.collect([
-            {
-              tokenId: BigInt(position.id),
-              recipient: wallet.account as `0x${string}`,
-              amount0Max: MAX_UINT128,
-              amount1Max: MAX_UINT128,
-            },
-          ]);
 
-          const fees0USD = CurrencyAmount.fromRawAmount(
-            unwrappedToken(
-              new AlgebranToken(
-                wallet.currentChainId,
-                pool.token0.id,
-                Number(pool.token0.decimals),
-                pool.token0.symbol,
-                pool.token0.name
-              )
-            ),
-            fees0.toString()
-          );
-
-          const fees1USD = CurrencyAmount.fromRawAmount(
-            unwrappedToken(
-              new AlgebranToken(
-                wallet.currentChainId,
-                pool.token1.id,
-                Number(pool.token1.decimals),
-                pool.token1.symbol,
-                pool.token1.name
-              )
-            ),
-            fees1.toString()
-          );
-
-          const fees0USDformatted = fees0USD
-            ? Number(fees0USD.toSignificant()) * Number(pool.token0.derivedUSD)
-            : 0;
-          const fees1USDformatted = fees1USD
-            ? Number(fees1USD.toSignificant()) * Number(pool.token1.derivedUSD)
-            : 0;
-
-          if (
-            fetchedPositions.includes(position.pool.id.concat("-", position.id))
-          )
-            return;
-
-          if (newPools[pool.id]) {
-            const existingPool = newPools[pool.id];
-            if (existingPool) {
-              existingPool.fees = existingPool.fees.plus(
-                BigNumber(fees0USDformatted + fees1USDformatted)
-              );
-            }
+        setPools((prev) => {
+          if (prev[pool.id]) {
+            return {
+              ...prev,
+              [pool.id]: {
+                ...prev[pool.id],
+                fees: prev[pool.id].fees.plus(
+                  BigNumber(fees0USDformatted + fees1USDformatted)
+                ),
+              },
+            };
           } else {
-            newPools[pool.id] = {
-              ...(pool as Pool),
-              fees: BigNumber(fees0USDformatted + fees1USDformatted),
+            return {
+              ...prev,
+              [pool.id]: {
+                ...(pool as Pool),
+                fees: BigNumber(fees0USDformatted + fees1USDformatted),
+              },
             };
           }
+        });
 
-          fetchedPositions.push(pool.id.concat("-", position.id));
-        } catch (error) {
-          console.error(error);
-        }
-      })
-    ).then(() => {
-      Object.entries(newPools).forEach(([id, pool]) => {
-        if (pools[id]) {
-          pools[id].fees = BigNumber(pools[id].fees.plus(pool.fees));
-        } else {
-          pools[id] = pool;
-        }
-      });
-
-      setIsLoading(false);
+        fetchedPositions.push(pool.id.concat("-", position.id));
+      } catch (error) {
+        console.error(error);
+      }
     });
+
+    setIsLoading(false);
   }, [algebraPositionManager.simulate, data, wallet.isInit]);
 
   return {
